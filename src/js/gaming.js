fb037e61c088ee8942a8a5d48a6e518dbb78c397
talog <tee.talog@gmail.com>
2020-05-03 13:05:12 +0900

    delete gaming

--- src/js/gaming.js
+++
@@ -1,9 +0,0 @@
-import state from "./state"
-import { configKeys, chatListFunc, isModerator, isUsername } from "./gaming-const"
-import initialize from "./modules/initialize"
-import defaultValues from "./default-values"
-
-state.chatListFunc = chatListFunc
-state.defaultColor = defaultValues.gaming
-
-initialize(configKeys, isModerator, isUsername)
